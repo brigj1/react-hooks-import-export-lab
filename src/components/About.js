c01ce399c95c4baeae49492a6c5552bac571a3aa
Brigham <brig.johnson@gmail.com>
2022-05-19 18:47:10 -0400

    commit tree 00f501b13f70ddb645aea3895b9a6b8f76148913

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,22 @@
 import React from "react";
 
+import {image} from "../data/user"
+
+const div2 = (
+  <div id="card1" className="card">
+    hello world
+  </div>
+);
+
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
       <img src={image} alt="I made this" />
+      {div2}
     </div>
   );
 }
+
+export default About;
